1d4ede878c1ce140d4d2a6f78c58e774aa779bc5
GangsuKim <gangsu1813@gmail.com>
2024-05-16 10:29:52 +0900

    Update CLIP4STR demo

--- adapting/clip4str.py
+++ adapting/clip4str.py
@@ -1,3 +1,5 @@
+# https://github.com/VamosC/CLIP4STR
+
 import os.path as osp
 import argparse
 
@@ -24,7 +26,7 @@ def main():
     parser.add_argument('--checkpoint', help="Model checkpoint (or 'pretrained=<model_id>')")
     parser.add_argument('--device', default='cuda')
     parser.add_argument('--model_type', default='L', help="Model type : 'L' or 'B'")
-    parser.add_argument('--data_type', default='GEN_HASH')  # Inference data type
+    parser.add_argument('--images_path', type=str, help='Images to read')
     parser.add_argument('--CAP', action='store_true')  # Inference data type
     parser.add_argument('--AA', action='store_true')  # Inference data type
 
@@ -32,10 +34,7 @@ def main():
     kwargs = parse_model_args(unknown)
     print(f'Additional keyword arguments: {kwargs}')
 
-    # Inference with DataFrame
-    DATA_ORIGIN_PATH = 'PATH_TO_INFERENCE_DATA'
-
-    # Define CAP model
+    # [CAP] Define CAP model
     if args.CAP:
         CAP_MODEL_PATH = 'PATH_TO_CAP_MODEL'
         CAP_model = CAPModel(71, 300, 500, 5, 6)
@@ -43,13 +42,15 @@ def main():
         CAP_model.to(args.device)
         CAP_model.eval()
 
-        word2tensor = WordToTensor2D(case_sensitive=False, pad_size=30, device=args.device)
+        word2tensor = WordToTensor1D(case_sensitive=False, pad_size=30, device=args.device)
         print(f'loading CAP checkpoint from {CAP_MODEL_PATH}')
     print(f'Using CAP is {args.CAP}')
 
+    # [Anti-Aliasing] Define AA layer
     apply_aa = args.AA
     print(f'Using Anti-Aliasing is {args.AA}')
 
+    # [CLIP4STR] load CLIP4STR model from checkpoint
     model_type = args.model_type
     weight_folder = 'PATH_TO_clip4str_MODEL'
     print(f'=================== Using CLIP4STR-{model_type} ===================')
@@ -65,53 +66,34 @@ def main():
     model = load_from_checkpoint(args.checkpoint, **kwargs).eval().to(args.device)
     img_transform = SceneTextDataModule.get_transform(model.hparams.img_size)
 
-    print(f"[{data_type}] inference {'w/ CAP' if args.CAP else ''}")
-    df = pd.read_csv(os.path.join(DATA_ORIGIN_PATH, f'{data_type}.csv'))
-
-    files = list(df['filename'])
-
-    # Load priors
-    normal_prior = np.load('./CAP/priors/CAPv2_NORMAL_PRIOR.npy')
+    # [CAP] Load character distribution
+    normal_prior = np.load('../distributions/CAPv2_NORMAL_PRIOR.npy')
     hash_prior = np.load('./CAP/priors/CAPv2_HASH_PRIOR.npy')
     ipv4_prior = np.load('./CAP/priors/CAPv2_IPv4_PRIOR.npy')
     ipv6_prior = np.load('./CAP/priors/CAPv2_IPv6_PRIOR.npy')
     mac_prior = np.load('./CAP/priors/CAPv2_MAC_PRIOR.npy')
 
-    res_dict = {'origin': [], 'predict': [], 'label': [], 'correct': []}
+    files = sorted([x for x in os.listdir(args.images_path) if x.endswith('png') or x.endswith('jpeg') or x.endswith('jpg')])
 
-    BATCH_SIZE = 256
-    # dataset = CAPDataSet(df=df, origin_path=DATA_ORIGIN_PATH, img_transform=img_transform, apply_aa=apply_aa)
-    dataset = CAPDataSet(df=df, origin_path=os.path.join(DATA_ORIGIN_PATH, data_type), img_transform=img_transform, apply_aa=apply_aa)
-    dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, num_workers=0, shuffle=False)
+    for fname in files:
+        # Load image and prepare for input
+        filename = os.path.join(args.images_path, fname)
+        image = Image.open(filename).convert('RGB')
+        image = img_transform(image).unsqueeze(0).to(args.device)
 
-    for images, labels in tqdm(dataloader):
-        images = images.to(args.device)
-        p = model(images)
-        pred, confidences = model.tokenizer.decode(p)
-
-        res_dict['origin'] += pred
+        p = model(image).softmax(-1)
+        pred, p = model.tokenizer.decode(p)
 
         if args.CAP:
-            logit = CAP_model(word2tensor.convert(pred))
+            logit = CAP_model(word2tensor.convert(pred[0]))
             logit_softmax = logit.softmax(dim=1)
             posterior = torch.matmul(logit_softmax, torch.Tensor(np.vstack([normal_prior, hash_prior, ipv4_prior, ipv6_prior, mac_prior])).to(args.device))
             p[:, :, 1:] *= (1 + SwapVocab2D(posterior, './CAP/VOCAB_CLIP4STR.npy')[:, 1:-2].unsqueeze(1).to(args.device))
-            pred, confidences = model.tokenizer.decode(p)
+            pred_cap, confidences = model.tokenizer.decode(p)
 
-        res_dict['predict'] += pred
-        res_dict['label'] += labels
-
-    correct_arr = []
-    for pred, label in zip(res_dict['predict'], res_dict['label']):
-        correct_arr.append(1 if pred.upper() == label.upper() else 0)
-
-    res_dict['correct'] = correct_arr
-
-    print(f'CER : {round((sum(correct_arr) / len(files)) * 100, 2)}%')
-    print()
-
-    pd.DataFrame(res_dict).to_csv(f'./results/CLIP4STR-{model_type}_FINE_PAPER_{data_type}{"_CAP" if args.CAP else ""}{"_AA" if apply_aa else ""}_res.csv')
-
+            print(f'{fname}: {pred[0]}, {pred_cap[0]}')
+        else:
+            print(f'{fname}: {pred[0]}')
 
 if __name__ == '__main__':
     main()
